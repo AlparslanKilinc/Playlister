76641d094207001228fdb62d341a94a7524c094f
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-12-30 16:49:20 -0500

    http routes update

--- client/src/store/store-request-api/index.js
+++ client/src/store/store-request-api/index.js
@@ -12,7 +12,7 @@
 import axios from 'axios'
 axios.defaults.withCredentials = true;
 const api = axios.create({
-    baseURL: 'https://playlister-api.onrender.com',
+    baseURL: 'https://playlister-api.onrender.com/api',
 })
 
 // THESE ARE ALL THE REQUESTS WE`LL BE MAKING, ALL REQUESTS HAVE A
