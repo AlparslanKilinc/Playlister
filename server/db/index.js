e1e7b3be5d42783ac5d68c1d2a2886e9350541a1
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-12-30 16:23:13 -0500

    db changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,6 @@
 
 const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config();
+require('dotenv').config({ path: '../.env' });
 
 mongoose
     .connect(process.env.DB_CONNECT, { useNewUrlParser: true })
