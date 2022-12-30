7dd02cf23be0394569711b3432cfdad28a5e230c
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-12-30 13:46:40 -0500

    fix reactdevtool bug

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {disableReactDevTools} from 'react-router-dom';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
 /// Disable react dev tools when in production 
-if(ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
