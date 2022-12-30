9f611187745d2700abe457526aada1993f8d6761
Alparslan Kilinc <kilincalparslan70@gmail.com>
2022-12-30 16:50:18 -0500

    get rid of get

--- server/index.js
+++ server/index.js
@@ -24,9 +24,7 @@ app.use('/auth', authRouter)
 const playlistsRouter = require('./routes/playlists-router')
 app.use('/api', playlistsRouter)
 
-app.get('/', function (req, res) {
-    res.render('index', {});
-  });
+
 // INITIALIZE OUR DATABASE OBJECT
 const db = require('./db')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
