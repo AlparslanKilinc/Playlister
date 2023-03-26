7fe457555e82157a57b16bf211d37e62a801d909
Alparslan Kilinc <kilincalparslan70@gmail.com>
2023-03-26 03:08:13 -0400

    change appbanner with logout

--- client/src/components/AppBanner.js
+++ client/src/components/AppBanner.js
@@ -3,88 +3,28 @@ import { Link } from 'react-router-dom'
 import AuthContext from '../auth';
 import { GlobalStoreContext } from '../store'
 import { useHistory } from 'react-router-dom'
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import AccountCircle from '@mui/icons-material/AccountCircle';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
 import logo from '../images/p_logo.png';
+import Button from '@mui/material/Button';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function AppBanner() {
     const { auth } = useContext(AuthContext);
     const { store } = useContext(GlobalStoreContext);
-    const [anchorEl, setAnchorEl] = useState(null);
-    const isMenuOpen = Boolean(anchorEl);
     store.history = useHistory();
 
-    const handleProfileMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
-
     const handleLogout = () => {
-        handleMenuClose();
         auth.logoutUser();
         store.clearTransaction();
     }
-    const menuId = 'primary-search-account-menu';
-    const loggedOutMenu = (
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleMenuClose}><Link to='/login/'>Login</Link></MenuItem>
-            <MenuItem onClick={handleMenuClose}><Link to='/register/'>Create New Account</Link></MenuItem>
-        </Menu>
-    );
-    const loggedInMenu = 
-        <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            id={menuId}
-            keepMounted
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            open={isMenuOpen}
-            onClose={handleMenuClose}
-        >
-            <MenuItem onClick={handleLogout}>Logout</MenuItem>
-        </Menu>        
-    let menu = loggedOutMenu;
-    if (auth.loggedIn) {
-        menu = loggedInMenu;
-    }
     
     function getAccountMenu(loggedIn) {
-        let userInitials = auth.getUserInitials();
         if (loggedIn)
-            return <Avatar>{userInitials}</Avatar>;
+            return <Button onClick={handleLogout} style={{color:'#143C9A'}}><LogoutIcon></LogoutIcon></Button>;
         else
-            return <AccountCircle/>;
+            return '';
     }
 
     return (
@@ -92,23 +32,11 @@ export default function AppBanner() {
             <AppBar position="static">
                 <Toolbar id='app-banner'>
                     <Link style={{ textDecoration: 'none'}} to={ auth.loggedIn? '/home/':'/'}>
-                        <img className="images" src={logo}/>   
+                        <img className="banner-logo" src={logo}/>   
                     </Link>
-                        <Box>
-                            <IconButton
-                                size="large"
-                                edge="end"
-                                aria-label="account of current user"
-                                aria-controls={menuId}
-                                aria-haspopup="true"
-                                onClick={handleProfileMenuOpen}
-                            >
-                                {getAccountMenu(auth.loggedIn)}
-                            </IconButton>
-                        </Box>
+                    {getAccountMenu(auth.loggedIn)}
                 </Toolbar>
             </AppBar>
-            {menu}
         </Box>
     );
 }
